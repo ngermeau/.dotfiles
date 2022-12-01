046355960b5932319b7c5fa8c57ce24f980692fe
Nicolas Germeau <ngermeau@icloud.com>
2022-12-01 18:34:49 +0100

    setting up basic environnement

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,27 +3,41 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug('nvim-lua/plenary.nvim')         
-Plug 'folke/tokyonight.nvim'
+Plug('nvim-treesitter/nvim-treesitter')
+Plug('folke/tokyonight.nvim')
 Plug('christoomey/vim-tmux-navigator')
 Plug('preservim/nerdtree')
 Plug('tpope/vim-commentary')
+Plug('tpope/vim-fugitive')
 Plug('nvim-telescope/telescope.nvim')
 Plug('mattn/emmet-vim')
-Plug 'nvim-lualine/lualine.nvim'
+Plug('nvim-lualine/lualine.nvim')
 Plug('neovim/nvim-lspconfig')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('hrsh7th/cmp-buffer')
-Plug('hrsh7th/cmp-path')
-Plug('hrsh7th/cmp-cmdline')
 Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
+Plug('prettier/vim-prettier')
+Plug('iamcco/markdown-preview.nvim', { ["do"] = 'cd app && yarn install' })
+Plug('rafamadriz/friendly-snippets')
 vim.call('plug#end')
+
+-- LEADER KEY -- 
+--
+vim.g.mapleader = ","                                                               -- leader key 
+
 
 -- CONFIGURATION LOADING -- 
 --
+require('treesitter-config')
+require('nerdtree-config')
 require('lsp-config')
 require('telescope-config')
-require('nerdtree-config')
 require('lualine').setup()
+require('nvim-cmp')
+require('prettier-config')
+require('markdown-preview-config')
 
 -- UI --
 --
@@ -47,11 +61,13 @@ vim.o.autoindent = true                             	                           
 vim.o.swapfile = false                                                              -- no swap file created when opening a file
 vim.o.hidden = true                                                                 -- allow unsaved buffers 
 vim.o.ignorecase = true                                                             -- make searches case-sensitive only if they contain upper-case characters
+vim.o.completeopt = menu,menuone,noselect
 
+vim.o.timeout = false                                                               -- do not timeout safter entering leader key
+vim.o.ttimeout = false 
 
 -- MAPPING --
 --
-vim.g.mapleader = ","                                                               -- leader key 
 vim.keymap.set('i','jj','<esc>')                                                    -- jj means escape (normal mode)
 vim.keymap.set({'n','i'},'<C-s>','<esc>:w<CR>')                                     -- go in normal mode and save 
 vim.keymap.set({'n','i'},'<C-w>','<esc>:q!<CR>')                                    -- close window with C-w
@@ -59,9 +75,6 @@ vim.keymap.set('n','<C-l>','<C-w>l')                                            
 vim.keymap.set('n','<C-h>','<C-w>h')                                                -- navigate window left  
 vim.keymap.set('n','<C-j>','<C-w>j')                                                -- navigate window down
 vim.keymap.set('n','<C-k>','<C-w>k')                                                -- navigate window up  
-
--- FILES --
---
-vim.keymap.set('n','<leader>fv',':e ~/.config/nvim/init.lua<CR>')
-
-
+vim.keymap.set('n','<leader>wo','<C-w>r')                                           -- rotate windows 
+vim.keymap.set('n','j','gj')                                                         -- move down even on multiple lines
+vim.keymap.set('n','k','gk')                                                         -- move up even on multiple lines
--- readme.md
+++ readme.md
@@ -8,13 +8,3 @@
 
 
 
-# Improvements 
-
-[x] ctrl+w to kill a vim window
-[ ] oceanic-next on alacritty
-[ ] oceanic-next on vim
-[ ] oceanic-next on tmux 
-[ ] start tmux with latest sessions
-- ctrl+a w to kill a pane 
-- map vim to nvim
-
